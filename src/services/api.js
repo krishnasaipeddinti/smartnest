c5a360bdcc8a4166de14ea745b9655730d3afc38
Krishna Sai <krishnasaipeddinti9393@gmail.com>
2026-06-15 18:30:05 +0530

    updated backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smartnest-production-fe64.up.railway.app/api",
 });
 
 API.interceptors.request.use(
